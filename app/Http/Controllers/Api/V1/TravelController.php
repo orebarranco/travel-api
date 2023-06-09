0817806aae7d4a9dc68b1ce3571979769a19fecf
Oreste Barranco Hernández <orebarranco90@gmail.com>
2023-06-09 16:43:28 -0400

    feat: add a Resource for Travel model
    Add TravelResource to transform the Travel eloquent model into a JSON
    response.
    
    Unnecessary data such as updated_at and created_at are removed
    from the response.
    
    The data number_of_nights is added.

--- app/Http/Controllers/Api/V1/TravelController.php
+++ app/Http/Controllers/Api/V1/TravelController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TravelResource;
 use App\Models\Travel;
 
 class TravelController extends Controller
 {
     public function index()
     {
-        return Travel::where('is_public', true)->get();
+        $travels = Travel::where('is_public', true)->get();
+
+        return TravelResource::collection($travels);
     }
 }
