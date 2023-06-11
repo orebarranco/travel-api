e75686d7c8cfb10644b948973c661b066b92c649
Oreste Barranco Hernández <orebarranco90@gmail.com>
2023-06-11 10:31:31 -0400

    feat: add a Resource for Tour model

--- app/Http/Controllers/Api/V1/TourController.php
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TourResource;
 use App\Models\Travel;
 
 class TourController extends Controller
 {
     public function index(Travel $travel)
     {
-        return $travel->tours()->orderBy('starting_date')->get();
+        $tours = $travel->tours()->orderBy('starting_date')->get();
+
+        return TourResource::collection($tours);
     }
 }
