6ac3d359223e0588d37cd35b7d6a01e32658474a
Oreste Barranco Hernández <orebarranco90@gmail.com>
2023-06-12 22:33:10 -0400

    feat: Add filter to list of paginated tours by travel
    Users can filter (search) the results by priceFrom, priceTo, dateFrom
    (from that startingDate) and dateTo (until that startingDate).
    
    User can sort the list by price asc and desc.

--- app/Http/Controllers/Api/V1/TourController.php
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -5,12 +5,30 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\TourResource;
 use App\Models\Travel;
+use Illuminate\Http\Request;
 
 class TourController extends Controller
 {
-    public function index(Travel $travel)
+    public function index(Travel $travel, Request $request)
     {
-        $tours = $travel->tours()->orderBy('starting_date')->paginate();
+        $tours = $travel->tours()
+            ->when($request->dateFrom, function ($query) use ($request) {
+                $query->where('starting_date', '>=', $request->dateFrom);
+            })
+            ->when($request->dateTo, function ($query) use ($request) {
+                $query->where('starting_date', '<=', $request->dateTo);
+            })
+            ->when($request->priceFrom, function ($query) use ($request) {
+                $query->where('price', '>=', $request->priceFrom * 100);
+            })
+            ->when($request->priceTo, function ($query) use ($request) {
+                $query->where('price', '<=', $request->priceTo * 100);
+            })
+            ->when($request->sortBy && $request->sortOrder, function ($query) use ($request) {
+                $query->orderBy($request->sortBy, $request->sortOrder);
+            })
+            ->orderBy('starting_date')
+            ->paginate();
 
         return TourResource::collection($tours);
     }
