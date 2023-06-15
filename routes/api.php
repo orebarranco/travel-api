23525086aac9b713ee3d7076c57334dadc8be359
Oreste Barranco Hernández <orebarranco90@gmail.com>
2023-06-14 23:41:22 -0400

    feat: add a private endpoint to create new travels

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Admin;
 use App\Http\Controllers\Api\V1\Auth\LoginController;
 use App\Http\Controllers\Api\V1\TourController;
 use App\Http\Controllers\Api\V1\TravelController;
@@ -25,3 +26,7 @@ Route::get('travels', [TravelController::class, 'index']);
 Route::get('travels/{travel:slug}/tours', [TourController::class, 'index']);
 
 Route::post('login', LoginController::class);
+
+Route::prefix('admin')->middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    Route::post('travels', [Admin\TravelController::class, 'store']);
+});
