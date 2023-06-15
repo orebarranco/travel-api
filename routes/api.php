191ec1f2dae8684a0a598a5b1692d80314a99bf5
Oreste Barranco Hernández <orebarranco90@gmail.com>
2023-06-14 22:43:19 -0400

    feat: add a public login endpoint for users

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Auth\LoginController;
 use App\Http\Controllers\Api\V1\TourController;
 use App\Http\Controllers\Api\V1\TravelController;
 use Illuminate\Http\Request;
@@ -22,3 +23,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('travels', [TravelController::class, 'index']);
 Route::get('travels/{travel:slug}/tours', [TourController::class, 'index']);
+
+Route::post('login', LoginController::class);
